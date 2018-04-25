dddf135e621dc627674f0b0358ef64cad8064f47
Stephen Weatherford <saweather@hotmail.com>
2018-04-25 13:18:16 -0700

    fix

--- test/index.ts
+++ test/index.ts
@@ -15,8 +15,7 @@
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
 
-// tslint:disable-next-line:no-var-requires
-let testRunner = require('vscode/lib/testrunner');
+import * as testRunner from "vscode/lib/testrunner";
 
 // You can directly control Mocha options by uncommenting the following lines
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
