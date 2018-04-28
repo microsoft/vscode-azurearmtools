cb0d594272143761f7df3fd983ac62206c010705
Stephen Weatherford <Stephen.Weatherford@microsoft.com>
2018-04-27 17:37:10 -0700

    Fix callstack and errorType of logError, fixes #78

--- src/AzureRMTools.ts
+++ src/AzureRMTools.ts
@@ -430,6 +430,7 @@ export class AzureRMTools {
 
         this.logOnError(() => {
             const deploymentTemplate = this.getDeploymentTemplate(document);
+            throw new Error("hi");
             if (deploymentTemplate) {
                 const context = deploymentTemplate.getContextFromDocumentLineAndColumnIndexes(position.line, position.character);
                 if (context.hoverInfo) {
@@ -792,17 +793,11 @@ export class AzureRMTools {
     private logError(eventName: string, error: any): void {
         const event: Telemetry.Event = {
             eventName: eventName,
-            errorType: typeof error
+            errorType: (typeof error === "object" && error.constructor) ? error.constructor.name : typeof error,
         };
 
-        if (error instanceof TypeError) {
-            event.message = error.message;
-            event.stack = error.stack;
-        } else {
-            // tslint:disable-next-line:no-for-in // Grandfathered in
-            for (const propertyName in error) {
-                event[propertyName] = error[propertyName];
-            }
+        for (const propertyName of Object.getOwnPropertyNames(error)) {
+            event[propertyName] = error[propertyName];
         }
 
         this.log(event);
