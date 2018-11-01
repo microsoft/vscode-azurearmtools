d5b268c5c8202fcf1de9e7f93a6e898bc089ab6d
Eric Jizba <erijiz@microsoft.com>
2018-11-01 16:23:27 -0700

    Move from travis ci to azure pipelines

--- gulpfile.js
+++ gulpfile.js
@@ -5,42 +5,17 @@
 
 const gulp = require('gulp');
 const path = require('path');
-const azureStorage = require('azure-storage');
-const vsce = require('vsce');
-const packageJson = require('./package.json');
+const cp = require('child_process');
 
-gulp.task('package', async () => {
-    await vsce.createVSIX();
+gulp.task('test', (cb) => {
+    const env = process.env;
+    env.DEBUGTELEMETRY = 1;
+    env.MOCHA_reporter = 'mocha-junit-reporter';
+    env.MOCHA_FILE = path.join(__dirname, 'test-results.xml');
+    const cmd = cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
+    cmd.on('close', (code) => {
+        cb(code);
+    });
 });
 
-gulp.task('upload-vsix', (callback) => {
-    if (process.env.TRAVIS_PULL_REQUEST_BRANCH) {
-        console.log('Skipping upload-vsix for PR build.');
-    } else {
-        const containerName = packageJson.name;
-        const vsixName = `${packageJson.name}-${packageJson.version}.vsix`;
-        const blobPath = path.join(process.env.TRAVIS_BRANCH, process.env.TRAVIS_BUILD_NUMBER, vsixName);
-        const blobService = azureStorage.createBlobService(process.env.STORAGE_NAME, process.env.STORAGE_KEY);
-        blobService.createContainerIfNotExists(containerName, { publicAccessLevel: "blob" }, (err) => {
-            if (err) {
-                callback(err);
-            } else {
-                blobService.createBlockBlobFromLocalFile(containerName, blobPath, vsixName, (err) => {
-                    if (err) {
-                        callback(err);
-                    } else {
-                        const brightYellowFormatting = '\x1b[33m\x1b[1m%s\x1b[0m';
-                        const brightWhiteFormatting = '\x1b[1m%s\x1b[0m';
-                        console.log();
-                        console.log(brightYellowFormatting, '================================================ vsix url ================================================');
-                        console.log();
-                        console.log(brightWhiteFormatting, blobService.getUrl(containerName, blobPath));
-                        console.log();
-                        console.log(brightYellowFormatting, '==========================================================================================================');
-                        console.log();
-                    }
-                });
-            }
-        });
-    }
-});
+
