194ac50d257a8af8535adb8d80bd92de20b02d07
Stephen Weatherford <stephwe@microsoft.com>
2019-05-01 12:39:23 -0700

    Don't break other installed extensions when running with no webpack

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@
 import * as path from 'path';
 
 // tslint:disable-next-line: strict-boolean-expressions
-export const isWebpack: boolean = /^(false|0)?$/i.test(process.env.AZCODE_IGNORE_BUNDLE || '');
+export const isWebpack: boolean = /^(false|0)?$/i.test(process.env.AZCODE_ARM_IGNORE_BUNDLE || '');
 
 export const assetsPath = path.join(__dirname, isWebpack ? "" : "..", "..", "assets");
 export const iconsPath = path.join(__dirname, isWebpack ? "" : "..", "..", "icons");
