93d8ae2ac4e1ceca3449e0460ac4006af6f569c7
Stephen Weatherford <stephwe@microsoft.com>
2019-05-08 12:16:48 -0700

    increase def timeout

--- gulpfile.ts
+++ gulpfile.ts
@@ -41,6 +41,7 @@ interface IExpressionMetadata {
 function test(): cp.ChildProcess {
     env.DEBUGTELEMETRY = '1';
     env.CODE_TESTS_PATH = path.join(__dirname, 'dist/test');
+    env.MOCHA_enableTimeouts = "4000";
     return cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
 }
 
