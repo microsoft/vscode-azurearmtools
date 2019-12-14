1888d0a048e0cb9d63b22c1fbada33e8a423bfe9
Stephen Weatherford <stephwe@microsoft.com>
2019-12-13 16:41:24 -0800

    PR fixes

--- src/util/wrapError.ts
+++ src/util/wrapError.ts
@@ -9,8 +9,8 @@ import { parseError } from 'vscode-azureextensionui';
 /**
  * "Wraps" an existing throwable item, placing a new message at the beginning of the current message.
  * Retains original stack info and other properties
+ * @param outerMessage Message to wrap the original message with
  * @param innerError Original error or other item that was thrown
- * @param wrappingMessage Message to wrap the original message with
  */
 export function wrapError(outerMessage: string, innerError: unknown): Error {
     const newMessage = outerMessage + os.EOL + parseError(innerError).message;
@@ -28,8 +28,7 @@ function cloneError(error: Error): Error {
     const copy = new Error();
     for (const propName of Object.getOwnPropertyNames(error)) {
         try {
-            const value = error[propName];
-            copy[propName] = value;
+            copy[propName] = error[propName];
         } catch (err2) {
             // Ignore
         }
