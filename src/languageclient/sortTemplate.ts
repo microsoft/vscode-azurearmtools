b9a97a71f8c2b7de243f6ee6a60478dfc3bf140d
Nils Hedström <nizze@lysator.liu.se>
2019-12-22 22:44:03 +0100

    Also sorts parameters

--- src/languageclient/sortTemplate.ts
+++ src/languageclient/sortTemplate.ts
@@ -3,49 +3,94 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// import { TextDocument, window } from 'vscode';
 import * as os from 'os';
 import * as vscode from "vscode";
 import { DeploymentTemplate } from "../DeploymentTemplate";
 import { ext } from '../extensionVariables';
-import * as Json from "../JSON";
+import { IParameterDefinition } from '../IParameterDefinition';
+import * as language from "../Language";
+import { IVariableDefinition } from '../VariableDefinition';
 
 export async function sortTemplate(template: DeploymentTemplate | undefined): Promise<void> {
     if (template === undefined) {
         return;
     }
     ext.outputChannel.appendLine("Sorting template");
-    let variables = template.topLevelScope.variableDefinitions;
-    let start = variables[0].span.startIndex;
-    let end = variables[variables.length - 1].span.afterEndIndex;
-    let textEditor = vscode.window.activeTextEditor;
-    if (textEditor === undefined) {
-        return;
-    }
-    let document = textEditor.document;
-    if (document === undefined) {
-        return;
-    }
-    let position = document.positionAt(start);
-    if (position === undefined) {
-        return;
-    }
-    let sorted = variables.sort((a, b) => a.nameValue.quotedValue.localeCompare(b.nameValue.quotedValue));
-    let startPosition = document.positionAt(start);
-    let startOfLine = new vscode.Position(startPosition.line, 0);
-    let selection = new vscode.Selection(document.positionAt(start), document.positionAt(end));
-    let indentText = document.getText(new vscode.Range(startOfLine, startPosition)).replace(/\S/g, ' ');
-    let joined = sorted.map(x => `${x.nameValue.quotedValue}: ${toString(x.value)}`).join(`,${os.EOL}${indentText}`);
-    textEditor.edit(x => x.replace(selection, joined));
+    await sortVariables(template);
+    await sortParameters(template);
     vscode.window.showInformationMessage("Done sorting template!");
 }
 
-function toString(value: Json.Value | null): String {
-    if (value === null) {
-        return "null";
+function sortVariables(template: DeploymentTemplate): Thenable<boolean> {
+    return sortGeneric<IVariableDefinition>(
+        template.topLevelScope.variableDefinitions,
+        x => x.nameValue.quotedValue, x => x.span);
+}
+
+function sortParameters(template: DeploymentTemplate): Thenable<boolean> {
+    return sortGeneric<IParameterDefinition>(
+        template.topLevelScope.parameterDefinitions,
+        x => x.nameValue.quotedValue, x => x.fullSpan);
+}
+
+function sortGeneric<T>(list: T[], sortSelector: (value: T) => string, spanSelector: (value: T) => language.Span): Thenable<boolean> {
+    let promise = Promise.resolve(true);
+    let textEditor = vscode.window.activeTextEditor;
+    if (textEditor === undefined || list.length < 2) {
+        return promise;
     }
-    if (value.valueKind === Json.ValueKind.StringValue) {
-        return `"${value.toFriendlyString()}"`;
+    let document = textEditor.document;
+    let selection = getSelection(spanSelector(list[0]), spanSelector(list[list.length - 1]), document);
+    let orderBefore = list.map(sortSelector);
+    let sorted = list.sort((a, b) => sortSelector(a).localeCompare(sortSelector(b)));
+    let orderAfter = list.map(sortSelector);
+    if (arraysEqual<string>(orderBefore, orderAfter)) {
+        return promise;
     }
-    return value.toFriendlyString();
+    let indentText = getIndentText(spanSelector(list[0]), document);
+    let joined = sorted.map(x => getText(spanSelector(x), document)).join(`,${os.EOL}${indentText}`);
+    return textEditor.edit(x => x.replace(selection, joined));
+}
+
+function getIndentText(span: language.Span, document: vscode.TextDocument): String {
+    let start = span.startIndex;
+    let startPosition = document.positionAt(start);
+    let startOfLine = new vscode.Position(startPosition.line, 0);
+    let indentText = document.getText(new vscode.Range(startOfLine, startPosition)).replace(/\S/g, ' ');
+    return indentText;
+}
+
+function getRange(span: language.Span, document: vscode.TextDocument): vscode.Range {
+    return new vscode.Range(document.positionAt(span.startIndex), document.positionAt(span.afterEndIndex));
+}
+
+function getText(span: language.Span, document: vscode.TextDocument): String {
+    let range = getRange(span, document);
+    let text = document.getText(range);
+    return text;
+}
+
+function getSelection(start: language.Span, end: language.Span, document: vscode.TextDocument): vscode.Selection {
+    let startIndex = start.startIndex;
+    let endIndex = end.afterEndIndex;
+    let selection = new vscode.Selection(document.positionAt(startIndex), document.positionAt(endIndex));
+    return selection;
+}
+
+function arraysEqual<T>(a: T[], b: T[]): boolean {
+    if (a === b) {
+        return true;
+    }
+    if (a == null || b == null) {
+        return false;
+    }
+    if (a.length !== b.length) {
+        return false;
+    }
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
