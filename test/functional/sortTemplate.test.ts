2622a322df70365afc9e655a82e2a543df1219fa
Nils Hedström <nizze@lysator.liu.se>
2020-02-08 04:34:09 +0100

    Removed unsused variable

--- test/functional/sortTemplate.test.ts
+++ test/functional/sortTemplate.test.ts
@@ -7,7 +7,6 @@
 
 // WARNING: At the breakpoint, the extension will be in an inactivate state (i.e., if you make changes in the editor, diagnostics,
 //   formatting, etc. will not be updated until you F5 again)
-const DEBUG_BREAK_AFTER_INSERTING_SNIPPET = false;
 
 import * as assert from 'assert';
 import * as fse from 'fs-extra';
