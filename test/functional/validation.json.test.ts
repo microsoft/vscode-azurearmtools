87414f158998c0e6288075a987a4a6067caa35c9
Stephen Weatherford <stephwe@microsoft.com>
2020-03-04 19:06:29 -0800

    Add comments to a functional test

--- test/functional/validation.json.test.ts
+++ test/functional/validation.json.test.ts
@@ -10,6 +10,7 @@ suite("JSON validation", () => {
         await testDiagnostics(
             `{  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
             "contentVersion": "1.0.0.0",
+            // Some comments here
             "resources": []`,
             {
             },
