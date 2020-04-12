bb584cccf0745c2ea62442d846bfcd16d3f8d991
Hedström, Nils <nils.hedstrom@if.se>
2020-04-12 20:46:01 +0200

    Fixed white space changes in AzureRMTools.ts

--- src/AzureRMTools.ts
+++ src/AzureRMTools.ts
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------------------------
-*  Copyright (c) Microsoft Corporation. All rights reserved.
-*  Licensed under the MIT License. See License.md in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 // tslint:disable:promise-function-async max-line-length // Grandfathered in
 
