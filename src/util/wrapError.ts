b7b1aa0a0a5cbff6896c5d78fcc3cada186ad3da
Stephen Weatherford <stephwe@microsoft.com>
2020-04-16 11:30:29 -0700

    Fix tests

--- src/util/wrapError.ts
+++ src/util/wrapError.ts
@@ -13,6 +13,8 @@ import { parseError } from 'vscode-azureextensionui';
  * @param innerError Original error or other item that was thrown
  */
 export function wrapError(outerMessage: string, innerError: unknown): Error {
+    // Note: We add a space as well as an EOL because in some vscode scenarios the EOL
+    //   doesn't show up in the UI
     // tslint:disable-next-line:prefer-template
     const newMessage = outerMessage + " " + os.EOL + parseError(innerError).message;
 
