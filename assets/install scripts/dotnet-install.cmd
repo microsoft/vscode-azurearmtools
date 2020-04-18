ebd8586b954637360d1caa3517631ad7c75770b9
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2020-04-17 22:54:10 -0700

    Support TLS12/13 (#620)
    Co-authored-by: Stephen Weatherford <saweather@hotmail.com>

--- assets/install scripts/dotnet-install.cmd
+++ assets/install scripts/dotnet-install.cmd
@@ -1,2 +1,2 @@
 @ECHO OFF
-PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0dotnet-install.ps1' %*; exit $LASTEXITCODE"
+PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = ''; [System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12+[System.Net.SecurityProtocolType]::Tls13; & '%~dp0dotnet-install.ps1' %*; exit $LASTEXITCODE"
