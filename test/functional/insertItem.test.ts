e2700579553b2daf21caefaa9b1083d633d025aa
Hedström, Nils <nils.hedstrom@if.se>
2020-04-25 19:05:35 +0200

    Fixed failing unit tests

--- test/functional/insertItem.test.ts
+++ test/functional/insertItem.test.ts
@@ -168,8 +168,10 @@ suite("InsertItem", async (): Promise<void> => {
             "type": "Microsoft.Network/applicationSecurityGroups",
             "apiVersion": "2019-11-01",
             "location": "[resourceGroup().location]",
-            "tags": {},
-            "properties": {}
+            "tags": {
+            },
+            "properties": {
+            }
         }
     ]
 }`;
