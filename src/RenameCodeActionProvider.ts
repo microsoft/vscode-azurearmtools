c07b8ecc1c7e9114dd9a9f52f789470bb0f276af
Hedström, Nils <nils.hedstrom@if.se>
2020-05-24 11:34:19 +0200

    Showing the 'Extract parameter' code action when value is selected

--- src/RenameCodeActionProvider.ts
+++ src/RenameCodeActionProvider.ts
@@ -1,6 +1,9 @@
 
 import * as vscode from 'vscode';
+import { Property } from './JSON';
+import { Contains } from './Language';
 import { PositionContext } from './PositionContext';
+import { TemplatePositionContext } from './TemplatePositionContext';
 import { getRenameError } from './util/getRenameError';
 const command = 'editor.action.rename';
 export class RenameCodeActionProvider implements vscode.CodeActionProvider {
@@ -9,11 +12,22 @@ export class RenameCodeActionProvider implements vscode.CodeActionProvider {
     }
 
     public async provideCodeActions(document: vscode.TextDocument, range: vscode.Range): Promise<vscode.CodeAction[] | undefined> {
-        let pc = await this.action(document, range.start);
+        let pc = await this.action(document, range.start) as TemplatePositionContext;
         if (!pc) {
             return;
         }
         const referenceSiteInfo = pc.getReferenceSiteInfo(true);
+        if (range.start.line === range.end.line && range.start.character !== range.end.character) {
+            let jsonToken = pc.document.getJSONValueAtDocumentCharacterIndex(pc.jsonTokenStartIndex - 1, Contains.extended);
+            if (jsonToken instanceof Property) {
+                const selectedText = document.getText(range);
+                if (pc.jsonValue && jsonToken.value && jsonToken.value.span === pc.jsonValue.span && selectedText && pc.jsonValue.asStringValue?.unquotedValue === selectedText) {
+                    return [
+                        this.createExtractParameterCommand()
+                    ];
+                }
+            }
+        }
         if (!referenceSiteInfo || getRenameError(referenceSiteInfo)) {
             return;
         }
@@ -27,4 +41,11 @@ export class RenameCodeActionProvider implements vscode.CodeActionProvider {
         action.command = { command: command, title: '' };
         return action;
     }
+
+    private createExtractParameterCommand(): vscode.CodeAction {
+        const action = new vscode.CodeAction('Extract Parameter...', vscode.CodeActionKind.RefactorExtract);
+        action.command = { command: command, title: '' };
+        return action;
+    }
+
 }
