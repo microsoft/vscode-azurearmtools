bfb42bd7082de2228cff839caab73b260be64770
Stephen Weatherford <stephwe@microsoft.com>
2020-07-29 13:07:46 -0700

    ALWAYS_ECHO_TEST_LOG

--- test/support/StringTestLog.ts
+++ test/support/StringTestLog.ts
@@ -4,11 +4,16 @@
 
 import { ITestLog } from "./ITestLog";
 
-export class StringLog implements ITestLog {
+const alwaysEchoTestLog: boolean = /^(true|1)?$/i.test(process.env.ALWAYS_ECHO_TEST_LOG ?? '');
+
+export class StringTestLog implements ITestLog {
     private _data: string[] = [];
 
     public writeLine(message: string): void {
         this._data.push(message);
+        if (alwaysEchoTestLog) {
+            console.log(`testLog: ${message}`);
+        }
     }
 
     public writeLineIfLogCreated(message: string): void {
