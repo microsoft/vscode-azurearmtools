f560e412d107e6f6df2a15a0579794e133b3a6bb
Stephen Weatherford <stephwe@microsoft.com>
2020-09-15 20:06:22 -0700

    timeout 10000

--- gulpfile.ts
+++ gulpfile.ts
@@ -79,7 +79,7 @@ interface IExpressionMetadata {
 function test(): cp.ChildProcess {
     env.DEBUGTELEMETRY = 'verbose';
     env.CODE_TESTS_PATH = path.join(__dirname, 'dist/test');
-    env.MOCHA_timeout = "120000";
+    env.MOCHA_timeout = "10000";
     env.MOCHA_enableTimeouts = "1";
     return cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
 }
