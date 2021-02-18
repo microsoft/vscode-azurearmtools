9b94b1680954213282f732fd9336764fd2850f61
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2021-02-17 22:25:57 -0800

    Add msg to fix other validation errors (#1194)
    * Add msg to fix other validation errors
    
    * Change another msg
    
    Co-authored-by: Stephen Weatherford <stephen.weatherford@microsoft.com>

--- src/AzureRMTools.ts
+++ src/AzureRMTools.ts
@@ -25,6 +25,7 @@ import { ExtractItem } from './documents/templates/ExtractItem';
 import { getNormalizedDocumentKey } from './documents/templates/getNormalizedDocumentKey';
 import { gotoResources } from './documents/templates/gotoResources';
 import { getItemTypeQuickPicks, InsertItem } from "./documents/templates/insertItem";
+import { LinkedFileLoadState } from './documents/templates/linkedTemplates/LinkedFileLoadState';
 import { assignTemplateGraphToDeploymentTemplate, INotifyTemplateGraphArgs, openLinkedTemplateFile } from './documents/templates/linkedTemplates/linkedTemplates';
 import { allSchemas, getPreferredSchema } from './documents/templates/schemas';
 import { getQuickPickItems, sortTemplate } from "./documents/templates/sortTemplate";
@@ -1752,6 +1753,11 @@ export class AzureRMTools implements IProvideOpenedDocuments {
 
             const rootTemplateUri = vscode.Uri.parse(e.rootTemplateUri);
             const rootTemplate = this.getOpenedDeploymentTemplate(rootTemplateUri);
+
+            // tslint:disable-next-line: no-console asdf
+            console.log(`onTemplateGraphAvailable: ${path.basename(e.rootTemplateUri)}, isComplete=${e.isComplete}:`);
+            // tslint:disable-next-line: no-console
+            console.log(e.linkedTemplates.map(lt => `    ${path.basename(lt.fullUri)}: ${LinkedFileLoadState[lt.loadState]} ${lt.loadErrorMessage ?? ''}`).join('\n'));
 
             // Cache the template graph results
             const rootTemplateKey = getNormalizedDocumentKey(vscode.Uri.parse(e.rootTemplateUri, true));
--- src/documents/templates/deploymentTemplateCodeLenses.ts
+++ src/documents/templates/deploymentTemplateCodeLenses.ts
@@ -253,9 +253,9 @@ export class LinkedTemplateCodeLens extends ResolvableCodeLens {
             if (!hasParameterFile) {
                 title += " " + "(validation disabled)";
             } else if (firstLinkedTemplateRef) {
-                title += " " + "(validation enabled)";
+                // title += " " + "(validation enabled)";
             } else {
-                title += " " + "(waiting for language server)";
+                title += " " + "(cannot validate - make sure all other validation errors have been fixed)";
             }
         } else {
             title = "Linked template  ($(warning) Validation with uri not yet supported, consider using relativePath property)";
