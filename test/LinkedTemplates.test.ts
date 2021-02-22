973dc928ecd1376505c01d3a9b6cb5c15e482542
Stephen Weatherford <stephen.weatherford@microsoft.com>
2021-02-22 15:30:50 -0800

    Add 1185 test

--- test/LinkedTemplates.test.ts
+++ test/LinkedTemplates.test.ts
@@ -75,58 +75,55 @@ suite("Linked templates regressions", () => {
         // TODO: For some reason, these two tests are failing consistently in the
         // Windows build pipeline, but not locally or on other platforms.
         if (!isWin32) {
-
-            // tslint:disable-next-line: no-suspicious-comment
-            /* TODO: Regressed by https://github.com/microsoft/vscode-azurearmtools/issues/1185
-                testWithLanguageServer("Nested template wrong param type", async () => {
-                    await testDiagnostics(
-                        {
-                            "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
-                            "contentVersion": "1.0.0.0",
-                            "resources": [
-                                {
-                                    "type": "Microsoft.Resources/deployments",
-                                    "apiVersion": "2019-10-01",
-                                    "name": "inner",
-                                    "properties": {
-                                        "expressionEvaluationOptions": {
-                                            "scope": "inner"
-                                        },
-                                        "mode": "Incremental",
+            testWithLanguageServer("Nested template wrong param type", async () => {
+                await testDiagnostics(
+                    {
+                        "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+                        "contentVersion": "1.0.0.0",
+                        "resources": [
+                            {
+                                "type": "Microsoft.Resources/deployments",
+                                "apiVersion": "2019-10-01",
+                                "name": "inner",
+                                "properties": {
+                                    "expressionEvaluationOptions": {
+                                        "scope": "inner"
+                                    },
+                                    "mode": "Incremental",
+                                    "parameters": {
+                                        "parameter1": {
+                                            "value": 123
+                                        }
+                                    },
+                                    "template": {
+                                        "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+                                        "contentVersion": "1.0.0.0",
                                         "parameters": {
                                             "parameter1": {
-                                                "value": 123
+                                                "type": "string"
                                             }
                                         },
-                                        "template": {
-                                            "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
-                                            "contentVersion": "1.0.0.0",
-                                            "parameters": {
-                                                "parameter1": {
-                                                    "type": "string"
-                                                }
-                                            },
-                                            "resources": []
-                                        }
+                                        "resources": []
                                     }
                                 }
-                            ]
-                        },
-                        {
-                            includeRange: true,
-                            parameters: {
-                                "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#",
-                                "contentVersion": "1.0.0.0",
-                                "parameters": {
-                                }
                             }
-                        },
-                        [
-                            "Error: Template validation failed: Template parameter JToken type is not valid. Expected 'String, Uri'. Actual 'Integer'. Please see https://aka.ms/arm-deploy/#parameter-file for usage details. (arm-template (validation)) [9,21-9,21] [The error occurred in a nested template near here] [13,19-13,19]",
-                            "Warning: The parameter 'parameter1' is never used. (arm-template (expressions)) [23,13-23,25]"
                         ]
-                    );
-                });*/
+                    },
+                    {
+                        includeRange: true,
+                        parameters: {
+                            "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#",
+                            "contentVersion": "1.0.0.0",
+                            "parameters": {
+                            }
+                        }
+                    },
+                    [
+                        "Error: Template validation failed: Template parameter JToken type is not valid. Expected 'String, Uri'. Actual 'Integer'. Please see https://aka.ms/arm-deploy/#parameter-file for usage details. (arm-template (validation)) [9,21-9,21] [The error occurred in a nested template near here] [23,21-23,21]",
+                        "Warning: The parameter 'parameter1' is never used. (arm-template (expressions)) [23,13-23,25]"
+                    ]
+                );
+            });
 
             testWithLanguageServer("Nested template missing properties", async () => {
                 await testDiagnostics(
