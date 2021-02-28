456a3875e454cb4142192c2bceadddaba69bf87b
Stephen Weatherford <stephen.weatherford@microsoft.com>
2021-02-27 19:16:27 -0800

    fix nesting

--- test/functional/validation.regression.test.ts
+++ test/functional/validation.regression.test.ts
@@ -646,18 +646,18 @@ suite("Validation regression tests", () => {
             [
                 "Warning: The variable 'someTag' is never used. (arm-template (expressions)) [7,9-7,18]"
             ]);
+    });
 
-        // https://github.com/microsoft/vscode-azurearmtools/issues/967
-        testWithLanguageServer(`#967 Validation needs to recognize new top-level "scope" property `, async () => {
-            await testDiagnostics(
-                'templates/regression/967-nested-with-scope-property.json',
-                {
-                    parametersFile: 'templates/regression/967-nested-with-scope-property.parameters.json',
-                    includeSources: [diagnosticSources.backendValidation]
-                },
-                [
-                ]);
-        });
+    // https://github.com/microsoft/vscode-azurearmtools/issues/967
+    testWithLanguageServer(`#967 Validation needs to recognize new top-level "scope" property `, async () => {
+        await testDiagnostics(
+            'templates/regression/967-nested-with-scope-property.json',
+            {
+                parametersFile: 'templates/regression/967-nested-with-scope-property.parameters.json',
+                includeSources: [diagnosticSources.backendValidation]
+            },
+            [
+            ]);
     });
 
     // https://github.com/microsoft/vscode-azurearmtools/issues/967
