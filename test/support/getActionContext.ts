bf83210f085e4619ff796ae3e6c4da3141e1739d
Stephen Weatherford <stephen.weatherford@microsoft.com>
2021-08-18 14:36:59 -0700

    Update to ui 0.37.0

--- test/support/getActionContext.ts
+++ test/support/getActionContext.ts
@@ -1,4 +1,5 @@
-import { IActionContext } from "vscode-azureextensionui";
+import { EventEmitter, InputBoxOptions, MessageItem, OpenDialogOptions, QuickPickItem, Uri } from "vscode";
+import { IActionContext, IAzureQuickPickOptions, PromptResult } from "vscode-azureextensionui";
 
 export function getActionContext(): IActionContext {
     return {
@@ -13,6 +14,21 @@ export function getActionContext(): IActionContext {
             rethrow: false,
             suppressDisplay: true,
             suppressReportIssue: true
+        },
+        ui: {
+            onDidFinishPrompt: new EventEmitter<PromptResult>().event,
+            showInputBox: async (options: InputBoxOptions): Promise<string> => {
+                throw new Error("Not implemented");
+            },
+            showQuickPick: async <T extends QuickPickItem>(items: T[] | Thenable<T[]>, options: IAzureQuickPickOptions): Promise<T> => {
+                throw new Error("Not implemented");
+            },
+            showWarningMessage: async <T extends MessageItem>(message: string, ...items: T[]): Promise<T> => {
+                throw new Error("Not implemented");
+            },
+            showOpenDialog: async (options: OpenDialogOptions): Promise<Uri[]> => {
+                throw new Error("Not implemented");
+            }
         }
     };
 }
