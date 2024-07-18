021b5448b9d98aaa6e2379f8466072c920aaab94
Stephen Weatherford (MSFT) <stephwe@microsoft.com>
2024-07-17 17:22:21 -0700

    Update azureextensionui to 0.37

--- test/support/getActionContext.ts
+++ test/support/getActionContext.ts
@@ -1,3 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as sinon from "sinon";
 import { IActionContext } from "vscode-azureextensionui";
 
 export function getActionContext(): IActionContext {
@@ -13,6 +19,13 @@ export function getActionContext(): IActionContext {
             rethrow: false,
             suppressDisplay: true,
             suppressReportIssue: true
+        },
+        ui: {
+            onDidFinishPrompt: sinon.stub(),
+            showQuickPick: sinon.stub().resolves(),
+            showInputBox: sinon.stub(),
+            showWarningMessage: sinon.stub(),
+            showOpenDialog: sinon.stub(),
         }
     };
 }
